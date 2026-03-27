c1e5bf37f62b62a347af65cfbf4a76e303568913
ZenNeko <86463999+ZenNeko@users.noreply.github.com>
2026-03-27 16:53:15 +0700

    add 27/3

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    
     public float speed = 3f;
     private Rigidbody rb;
     private GameObject player;
 
-    void Start()
+    private void Awake()
     {
-
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        rb.velocity = direction * speed;
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -7,7 +8,24 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
+        InvokeRepeating(nameof(RandomSpawn),0,5);
+    }
 
+    void RandomSpawn()
+    {
+        var index = Random.Range(0, spawnPoints.Length);
+        var spawnPoint = spawnPoints[index];
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            RandomSpawn();
+            yield return null;
+            
+        }
     }
 
 }
